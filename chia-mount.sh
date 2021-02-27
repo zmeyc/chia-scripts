53040700fe4a85e49e151a334e38f181e67831b7
Andrey Fidrya <af@zabiyaka.com>
2021-02-28 02:04:02 +0300

    Add chia-mount

---
+++ chia-mount.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -euo pipefail
+
+APPNAME=chia-mount
+
+usage() {
+  if [ $# -eq 0 ]; then
+    echo ""
+    echo "${APPNAME}"
+    echo ""
+    echo "Usage:"
+    echo ""
+    printf "%s\n" "  ${APPNAME} <subcommand> [arguments]"
+    echo ""
+    echo "Where subcommand is:"
+    echo ""
+    echo "  add <device>   Add device to /etc/fstab and mount it by label."
+    echo "                 Be sure to check /etc/fstab and make neccessary"
+    echo "                 adjustments. Also create mount point and assign"
+    echo "                 access rights to current user. Perform initial"
+    echo "                 mounting. *** SHOULD BE CALLED ONCE ***"
+    echo ""
+    exit 1
+  fi
+}
+
+do_add() {
+  [ "$#" -eq 1 ] || fatal "Expected device name"
+  local dev_name="$1"
+  local label
+  label="$(e2label "${dev_name}")"
+  printf "Disk label: %s\n" "${label}"
+
+  local mountpoint
+  mountpoint="/mnt/${label}"
+
+  printf "Creating mountpoint: %s\n" "${mountpoint}"
+  mkdir -p "${mountpoint}"
+
+  echo "Updating /etc/fstab"
+  printf "LABEL=%s %s auto nosuid,nodev,nofail,noatime 0 0\n" "${label}" "${mountpoint}" >> /etc/fstab
+
+  printf "Mounting: %s\n" "${mountpoint}"
+  mount "${mountpoint}"
+
+  echo "Changing files ownership"
+  chown -R "$(id -u):$(id -g)" "${mountpoint}"
+
+  echo "Done."
+}
+
+[ "$#" -ge 1 ] || {
+  usage
+  exit 1
+}
+
+subcommand="$1"
+shift
+
+case "$subcommand" in
+add)
+  do_add "$@"
+  ;;
+*)
+  fatal "Unknown subcommand $subcommand"
+  ;;
+esac
+
